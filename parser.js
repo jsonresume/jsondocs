2d7e07a70ebe30ce6281531e224c0e3acc153e1e
thomasdavis <thomasalwyndavis@gmail.com>
2014-07-03 19:11:13 +1000

    clean up

--- parser.js
+++ parser.js
@@ -1,31 +1,43 @@
 var _ = require('lodash');
 var fs  = require('fs');
 
-var schema = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
 
-console.log(schema);
+var file = process.argv[2];
+if(!file) {
+	console.log('No json schema file specified');
+	return;
+}
+var schema = JSON.parse(fs.readFileSync(file, 'utf8'));
 
-var html = '';//'<style>.property {  font-family: Verdana; padding: 10px 10px 10px 20px;}.type{font-weight: bold;}.description { font-size: 10px; color: #ccc;}</style>';
+if(!schema){
+	console.log('Not a valid json schema');
+	return;
+}
 var propertyOpenHTML = function(property, propertyName, first, last) {
 	var comma = ',';
 	if(last) {
 		comma = '';
 	}
 	var html;
-	var description = property.description || 'No description';
+	var description = property.description || '';
+	if(description.length !== 0) {
+		description = '// ' + description;
+	}
 	if(propertyName) {
 		html = '<div class="property"><span class="name">' + propertyName + '</span>: ';
  	} else {
 		html = '<div class="property">';
  	}
 	if(property.type === 'object') {
-		html += '<span class="type">{</span> <span class="description">// '+description+'</span>';
+		html += '<span class="type">{</span> <span class="description">'+description+'</span>';
 
+	} else if (property.type ==='array' && property.items.type ==='object') {
+		html += '<span class="type">[{</span> <span class="description">'+description+'</span>';
 	} else if (property.type ==='array') {
-		html += '<span class="type">[</span> <span class="description">// '+description+'</span>';
+		html += '<span class="type">[</span> <span class="description">'+description+'</span>';
 
 	} else {
-		html += '<span class="type">' + property.type + '</span>'+comma+' <span class="description">// '+description+'</span>';
+		html += '<span class="type">' + property.type + '</span>'+comma+' <span class="description">'+description+'</span>';
 	}
 	return html;
 }
@@ -38,14 +50,15 @@ var propertyCloseHTML = function(property, propertyName, first, last) {
 	if(property.type === 'object') {
 		html += '<span class="type">}'+comma+'</span>';
 	};
-	if(property.type === 'array') {
+	if (property.type ==='array' && property.items.type ==='object') {
+		html += '<span class="type">}]'+comma+'</span>';
+	} else if(property.type === 'array') {
 		html += '<span class="type">]'+comma+'</span>';
 	};
 	html += '</div>'
 	return html;
 }
 var parseProperty = function (property) {
-	console.log(property.properties && property.properties.length);
 	var properties = [];
 	_.each(property.properties, function(nestedProperty, key){
 		properties.push({property: nestedProperty, key: key});
@@ -55,9 +68,6 @@ var parseProperty = function (property) {
 		if(property.items.properties) {
 			_.each(property.items.properties, function(nestedItemProperty, key){
 				properties.push({property: nestedItemProperty, key: key});
-				//html += propertyOpenHTML(nestedItemProperty, key);
-				//parseProperty(nestedItemProperty);
-				//html += propertyCloseHTML(nestedItemProperty, key);
 			})	
 		} else {
 			properties.push({property: property.items, key: null});
@@ -69,17 +79,20 @@ var parseProperty = function (property) {
 	
 		var last = (index+1 === properties.length ? true : false);
 		var first = (index === 0 ? true : false);
-		console.log(first, last);
 		html += propertyOpenHTML(property.property, property.key, first, last);
 		parseProperty(property.property);
 		html += propertyCloseHTML(property.property, property.key, first, last);
 
 	});
 };
-html += '<div class="property"><span class="type">{</span>';
 
+// Start building the html string
+var html = '<div class="json-schema">';
+html += '<div class="property"><span class="type">{</span>';
 parseProperty(schema);
 html += '<span class="type">}</span></div>';
+html += '</div>';
+// Finished
+fs.writeFileSync(file + '.html', html);
 
 
-fs.writeFileSync('test.html', html);
